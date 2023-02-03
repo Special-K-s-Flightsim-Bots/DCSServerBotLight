d380b20da84879c9f6c1120d2a592b0047cb41bf
karel26 <dcs.specialk@gmail.com>
2023-02-03 11:01:02 +0100

    Improved discord bans

--- plugins/cloud/commands.py
+++ plugins/cloud/commands.py
@@ -103,6 +103,8 @@ class CloudHandlerMaster(CloudHandlerAgent):
     async def master_bans(self):
         try:
             for ban in (await self.get('discord-bans')):
+                if int(ban['discord_id']) == self.bot.owner_id:
+                    continue
                 user: discord.User = await self.bot.fetch_user(ban['discord_id'])
                 await self.bot.guilds[0].ban(user, reason='DGSA: ' + ban['reason'])
         except aiohttp.ClientError:
